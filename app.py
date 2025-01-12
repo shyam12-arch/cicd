50361ce29614e401b77b67e090f273551ecc0c8c
Shyam tyagi <77968605+shyam12-arch@users.noreply.github.com>
2025-01-12 17:24:00 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def print_current_time():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current Time: {current_time}")
+
+if __name__ == "__main__":
+    print_current_time()
