0baf9259433e8e28748a7200379e347e2e33c1c4
shyam12-arch <77968605+shyam12-arch@users.noreply.github.com>
2025-01-12 18:10:43 +0530

    second time changed..

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def print_current_time():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"shyam.........Current Time: {current_time}")
+    print(f"Hi shyam this is second time updates.........Current Time: {current_time}")
 
 if __name__ == "__main__":
     print_current_time()
