f5a72c60eb21566e4fa6be68778b8ba8ac202b5d
shyam12-arch <77968605+shyam12-arch@users.noreply.github.com>
2025-01-12 18:02:46 +0530

    changes...done

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def print_current_time():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Current Time: {current_time}")
+    print(f"shyam.........Current Time: {current_time}")
 
 if __name__ == "__main__":
     print_current_time()
